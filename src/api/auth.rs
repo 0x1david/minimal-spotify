146c34bb7f772cdb688193dde37baa42ea787ec0
0x1david <bousi.david@gmail.com>
2023-10-25 22:11:56 +0200

    first auth finished process

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,10 +1,46 @@
-use std::sync::Arc;
-
 use base64::Engine;
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use reqwest::{Error, Response};
+use serde_derive::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
+use super::constants::{scope, auth_url};
+use serde_urlencoded;
+use serde_json::Value;
 
+#[derive(Deserialize, Serialize, Debug)]
+struct TokenResponse {
+    access_token: String,
+    refresh_token: String,
+    expires_in: usize,
+}
+
+async fn auth() -> Result<(), Box<dyn std::error::Error>>{
+    dotenv::dotenv().ok();
+    let client_id = std::env::var("CLIENT_ID").expect("No client ID in ENV.");
+    let redirect_uri = std::env::var("REDIRECT_URI").expect("No redirect URI in ENV.");
+    let scope = scope();
+    let auth_url = auth_url();
+
+    let code_verifier = generate_code_verifier(128);
+    let code_challenge = generate_code_challenge(&code_verifier);
+
+    let auth_url = format!(
+    "https://accounts.spotify.com/authorize?{}",
+    serde_urlencoded::to_string([
+        ("client_id", &client_id),
+        ("response_type", &"code".to_string()),
+        ("redirect_uri", &redirect_uri),
+        ("code_challenge_method", &"S256".to_string()),
+        ("code_challenge", &code_challenge),
+        ("scope", &scope),
+    ])?
+);
+    let authorization_code = todo!("Connect to frontend and acces url via web");
+    let token_response = exchange_code(&authorization_code, &code_verifier).await?;
+
+    let access_token = token_response.access_token.as_str().to_string();
+    let refresh_token = token_response.refresh_token.as_str().to_string();
+}
 
 /// Generates and returns the authorization URL for Spotify's Web API.
 ///
@@ -14,8 +50,8 @@ use sha2::{Digest, Sha256};
 ///
 /// # Panics
 /// - If `CLIENT_ID` or `REDIRECT_URI` environment variables are not set.
-async fn get_auth_url() -> Result<Arc<str>, Box<dyn std::error::Error>> {
-    dotenv::dotenv().ok(); // TODO: MOVE TO MAIN
+async fn get_auth_url() -> Result<String, Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
     let client_id = std::env::var("CLIENT_ID").expect("No client ID in ENV.");
     let redirect_uri = std::env::var("REDIRECT_URI").expect("No redirect URI in ENV.");
     let code_challenge = generate_code_challenge(&generate_code_verifier(128));
@@ -26,7 +62,7 @@ async fn get_auth_url() -> Result<Arc<str>, Box<dyn std::error::Error>> {
         redirect_uri,
         code_challenge
 );
-    Ok(Arc::from(auth_url))
+    Ok(auth_url)
 }
 
 /// Exchanges the authorization code for access and refresh tokens.
@@ -40,7 +76,7 @@ async fn get_auth_url() -> Result<Arc<str>, Box<dyn std::error::Error>> {
 ///
 //// # Returns
 /// - `Response` .
-async fn exchange_code(code: Arc<str>, code_verifier: Arc<str>) -> Result<Response, Error> {
+async fn exchange_code(code: &str, code_verifier: &str) -> Result<TokenResponse, Error> {
     let token_url = "https://accounts.spotify.com/api/token";
     let redirect_uri = std::env::var("REDIRECT_URI").expect("No redirect URI in ENV.");
     let params = [
@@ -55,13 +91,10 @@ async fn exchange_code(code: Arc<str>, code_verifier: Arc<str>) -> Result<Respon
         .post(token_url)
         .form(&params)
         .send()
-        .await
-        .unwrap()
-        .error_for_status()?;
+        .await?
+        .error_for_status()?.json::<TokenResponse>().await?;
     return Ok(res);
-
 }
-
 
 /// Generates a code challenge from a given code verifier.
 ///
@@ -70,12 +103,14 @@ async fn exchange_code(code: Arc<str>, code_verifier: Arc<str>) -> Result<Respon
 ///
 /// # Returns
 /// - An `Arc<str>` containing the code challenge.
-fn generate_code_challenge(input: &str) -> Arc<str>{
+fn generate_code_challenge(input: &str) -> String {
     let mut hasher = Sha256::new();
     hasher.update(input.as_bytes());
     let result = hasher.finalize();
-    base64::engine::general_purpose::URL_SAFE_NO_PAD.encode(result).into()
+    base64::engine::general_purpose::URL_SAFE_NO_PAD
+        .encode(result)
 }
+
 
 /// Generates a code verifier of the specified length.
 ///
@@ -84,18 +119,13 @@ fn generate_code_challenge(input: &str) -> Arc<str>{
 ///
 /// # Returns
 /// - An `Arc<str>` containing the code verifier.
-fn generate_code_verifier(length: usize) -> Arc<str>{
-    let code_verifier: String = thread_rng()
+fn generate_code_verifier(length: usize) -> String {
+    thread_rng()
         .sample_iter(&Alphanumeric)
         .take(length)
         .map(char::from)
-        .collect();
-    Arc::from(code_verifier)
-
+        .collect()
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -113,7 +143,11 @@ mod tests {
     fn test_generate_code_verifier() {
         let length = 128;
         let verifier = generate_code_verifier(length);
-        assert_eq!(verifier.len(), length, "Code verifier length should match the specified length");
+        assert_eq!(
+            verifier.len(),
+            length,
+            "Code verifier length should match the specified length"
+        );
     }
 
     #[test]
@@ -122,13 +156,15 @@ mod tests {
             .enable_all()
             .build()
             .unwrap();
-        rt.block_on(async {  
-
+        rt.block_on(async {
             let result = get_auth_url().await;
 
             assert!(result.is_ok(), "Should return Ok");
             let url = result.unwrap();
-            assert!(url.contains("https://accounts.spotify.com/authorize"), "URL should contain the authorization endpoint");
+            assert!(
+                url.contains("https://accounts.spotify.com/authorize"),
+                "URL should contain the authorization endpoint"
+            );
         });
     }
 }
--- src/api/constants.rs
+++ src/api/constants.rs
@@ -1,1 +1,24 @@
 extern crate dotenv;
+
+pub fn scope() -> String {
+        "user-modify-playback-state \
+        user-read-playback-state \
+        user-read-currently-playing \
+        streaming \
+        playlist-read-private \
+        playlist-read-collaborative \
+        playlist-modify-private \
+        playlist-modify-public \
+        user-follow-modify \
+        user-follow-read \
+        user-read-playback-position \
+        user-top-read \
+        user-read-recently-played \
+        user-library-modify \
+        user-library-read".to_string()
+}
+
+pub fn auth_url() -> String {
+    "https://accounts.spotify.com/authorize".to_string()
+}
+
